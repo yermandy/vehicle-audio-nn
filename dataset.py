65644c1fc29d9c642813a7e032335876765081e4
yermandy <yermaand@gmail.com>
2021-07-14 15:53:42 +0200

    naive model

---
+++ dataset.py
@@ -0,0 +1,140 @@
+import librosa
+import torch
+import torchaudio
+import numpy as np
+from torch.utils.data import Dataset
+
+from imblearn.under_sampling import RandomUnderSampler
+
+class VehicleDataset(Dataset):
+
+    def __init__(self,
+                 audio_file,
+                 labels_file,
+                 start_time=0,
+                 end_time=int(1e8),
+                 window_len=1.0,
+                 seed=np.random.randint(0, int(1e8)),
+                 sampling='random',
+                 use_offset=False):
+        
+        # window_len, start_time, end_time in seconds
+        self.window_len = window_len
+        self.start_time = start_time
+        self.end_time = end_time
+        self.use_offset = use_offset
+        self.seed = seed
+
+        # load and convert to mono
+        self.signal, self.sr = torchaudio.load(audio_file)
+        self.signal = self.signal.mean(0)
+        # signal = signal.numpy()
+
+        # load labels
+        self.events = np.loadtxt(labels_file)
+
+        self.split_signal()
+
+        print(f'all: {len(self.labels)} | positive: {sum(self.labels)}')
+
+        self.n_fft = 1024
+        self.hop_length = 128
+        self.n_mels = 64
+
+        self.transform = torchaudio.transforms.MelSpectrogram(
+            sample_rate=self.sr,
+            n_fft=self.n_fft,
+            hop_length=self.hop_length,
+            n_mels=self.n_mels
+        )
+
+    
+    def split_signal(self):
+        signal = self.signal
+        events = self.events.copy()
+
+        window_len = self.window_len
+        start_time = self.start_time
+        end_time = self.end_time
+
+        sr = self.sr
+
+        # crop signal
+        offset = np.random.uniform(0, window_len, 1)[0] if self.use_offset else 0
+        signal = signal[int((start_time + offset) * sr): end_time * sr + 1]
+
+        n_samples_per_window = int(sr * window_len)
+
+        # split signal to windows of size window_len [sec]
+        signals = signal.split(n_samples_per_window)
+
+        # remove last window
+        signals = signals[:-1]
+        signals = np.array([s.numpy() for s in signals])
+        
+        # load events in seconds, crop and convert to events in samples
+        events = events - start_time + offset
+        events = events[events < end_time]
+        events *= sr
+
+        labels = []
+        for i in range(len(signals)):
+            counts = (i * n_samples_per_window <= events) & \
+                (events < i * n_samples_per_window + n_samples_per_window)
+            counts = counts.sum()
+            labels.append(int(counts > 0))
+            # labels.append(counts)
+
+        labels = np.array(labels)
+        signals = np.array(signals)
+
+        # if sampling == 'random':
+            # sampler = RandomUnderSampler(random_state=self.seed)
+        # else:
+            # sampler = SMOTETomek(random_state=self.seed)
+
+        sampler = RandomUnderSampler(random_state=self.seed)
+        signals, labels = sampler.fit_resample(signals, labels)
+
+        '''
+        mask = labels != 0
+
+        pos_labels = labels[mask]
+        pos_signals = signals[mask]
+
+        neg_labels = labels[~mask]
+        neg_signals = signals[~mask]
+
+        np.random.seed(self.seed)
+
+        neg_random_idx = np.random.choice(range(len(neg_signals)), len(pos_labels))
+        neg_labels = neg_labels[neg_random_idx]
+        neg_signals = neg_signals[neg_random_idx]
+
+        labels = np.concatenate((pos_labels, neg_labels))
+        signals = np.concatenate((pos_signals, neg_signals))
+        # '''
+
+        self.labels = labels
+        self.signals = torch.tensor(signals)
+
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index, use_librosa=False):
+        signal = self.signals[index]
+
+        if use_librosa:
+            signal = self.signals[index].numpy()
+            features = librosa.feature.melspectrogram(
+                signal,
+                sr=self.sr,
+                n_fft=self.n_fft,
+                hop_length=self.hop_length,
+                n_mels=self.n_mels
+            )[np.newaxis]
+        else:
+            features = self.transform(signal).unsqueeze(0)
+
+        return features, self.labels[index]
---
+++ train.py
@@ -0,0 +1,87 @@
+from model import ResNet18
+from dataset import VehicleDataset
+
+import torch
+import torch.nn as nn
+
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from datetime import datetime
+
+
+def run(audio_file, labels_file, n_epochs=150, cuda=0):
+
+    device = torch.device(f'cuda:{cuda}' if torch.cuda.is_available() else 'cpu')
+
+    print(f'Running on {device}')
+
+    trn_dataset = VehicleDataset(audio_file, labels_file, start_time=60, end_time=25 * 60, use_offset=True)
+    trn_loader = DataLoader(trn_dataset, batch_size=64, shuffle=True)
+
+    val_dataset = VehicleDataset(audio_file, labels_file, start_time=25 * 60, seed=0)
+    val_loader = DataLoader(val_dataset, batch_size=64)
+
+    model = ResNet18().to(device)
+
+    loss = nn.CrossEntropyLoss()
+
+    optim = Adam(model.parameters(), lr=0.0001)
+
+    loop = tqdm(range(n_epochs))
+
+    val_correct_best = 0
+
+    uuid = int(datetime.now().timestamp())
+
+    for epoch in loop:
+
+        trn_correct = 0
+        val_correct = 0
+        trn_loss = 0
+
+        model.train()
+        for tensor, target in trn_loader:
+            
+            tensor = tensor.to(device)
+            target = target.to(device)
+            
+            scores = model(tensor)
+            loss_val = loss(scores, target)
+
+            preds = scores.argmax(1)
+
+            trn_loss += loss_val.detach()
+            trn_correct += (target == preds).sum()
+
+            optim.zero_grad()
+            loss_val.backward()
+            optim.step()
+
+        trn_loader.dataset.split_signal()
+        
+        model.eval()
+        with torch.no_grad():
+            for tensor, target in val_loader:
+                tensor = tensor.to(device)
+                target = target.to(device)
+
+                scores = model(tensor)
+                preds = scores.argmax(1)
+                val_correct += (target == preds).sum()
+
+        if val_correct > val_correct_best:
+            val_correct_best = val_correct
+            torch.save(model.state_dict(), f'models/model_{uuid}.pth')
+
+        loop.set_description(f'loss {trn_loss.item():.4f} | trn acc {trn_correct / len(trn_dataset):.4f} | val acc {val_correct / len(val_dataset):.4f} | best acc {val_correct_best / len(val_dataset):.4f}')
+
+
+if __name__ == "__main__":
+
+    audio_file = 'data/audio/20190819-Kutna Hora-L4-out-MVI_0040.wav'
+    labels_file = 'data/labels/20190819-Kutna Hora-L4-out-MVI_0040.txt'
+
+    for i in range(5):
+        run(audio_file, labels_file)
